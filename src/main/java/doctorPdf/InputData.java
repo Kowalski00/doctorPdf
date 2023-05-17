67ed7ca5e59c7fcf2bfc97dea4a7596a26a67648
kowalski00 <rk0191@hotmail.com>
2023-05-16 23:19:19 -0300

    Adds wrapper to pass input data from form screen

---
+++ src/main/java/doctorPdf/InputData.java
@@ -0,0 +1,28 @@
+package doctorPdf;
+
+public class InputData {
+	
+	private String patientFullName;
+	private String selectedPath;
+	private int fontSize;
+	
+	public String getPatientFullName() {
+		return patientFullName;
+	}
+	public void setPatientFullName(String patientFullName) {
+		this.patientFullName = patientFullName;
+	}
+	public String getSelectedPath() {
+		return selectedPath;
+	}
+	public void setSelectedPath(String selectedPath) {
+		this.selectedPath = selectedPath;
+	}
+	public int getFontSize() {
+		return fontSize;
+	}
+	public void setFontSize(int fontSize) {
+		this.fontSize = fontSize;
+	}
+	
+}
